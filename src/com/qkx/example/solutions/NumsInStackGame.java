00cff1b31cf873b83b5af7e30ceaf3f4a1503767
quekx <syqkx123@163.com>
2017-04-21 00:26:52 +0800

    ...

--- src/com/qkx/example/solutions/NumsInStackGame.java
+++ src/com/qkx/example/solutions/NumsInStackGame.java
@@ -1,5 +1,8 @@
 package com.qkx.example.solutions;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by qkx on 16/11/25.
  */
@@ -12,11 +15,10 @@ public class NumsInStackGame {
      */
 
     /**
-     *
      * @param stack 数字
      * @return 先拿是否能赢
      */
-    public boolean solve(int[] stack) {
+    public boolean canWin(int[] stack) {
         if (stack == null || stack.length <= 2) return true;
 
         int length = stack.length;
@@ -33,5 +35,54 @@ public class NumsInStackGame {
         }
 
         return dp[length - 1] * 2 > sum[length - 1];
+    }
+
+    public List<Integer> solve(int[] stack) {
+        List<Integer> result = new LinkedList<>();
+        if (stack == null) return result;
+        if (stack.length <= 2) {
+            for (int num : stack) {
+                result.add(num);
+            }
+            return result;
+        }
+
+        int length = stack.length;
+        int[] sum = new int[length];
+        for (int i = 1; i < length; i++) {
+            sum[i] = sum[i - 1] + sum[i];
+        }
+
+        int[] num = new int[length];
+        num[0] = 1;
+        num[1] = 2;
+        int[] dp = new int[length];
+        dp[0] = stack[0];
+        dp[1] = stack[0] + stack[1];
+        for (int i = 2; i < length; i++) {
+            int q1 = stack[i] + sum[i - 1] - dp[i - 1];
+            int q2 = stack[i] + stack[i - 1] + sum[i - 2] - dp[i - 2];
+            if (q1 >= q2) {
+                num[i] = 1;
+                dp[i] = q1;
+            } else {
+                num[i] = 2;
+                dp[i] = q2;
+            }
+        }
+
+        int index = length - 1;
+        boolean turn = true;
+        while (index >= 0) {
+            if (turn) {
+                for (int i = 0; i < num[index]; i++) {
+                    result.add(index - i);
+                }
+            }
+            index = index - num[index];
+            turn = !turn;
+        }
+
+        return result;
     }
 }
