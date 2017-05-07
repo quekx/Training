98bfa807783b602f50b871edfed7094d4c3d80dd
quekx <syqkx123@163.com>
2017-05-07 21:19:24 +0800

    LCS

--- src/com/qkx/example/exercise/algorithm/LCS.java
+++ src/com/qkx/example/exercise/algorithm/LCS.java
@@ -27,4 +27,29 @@ public class LCS {
         }
         return max;
     }
+
+    private static int longestCommonSequence(String s1, String s2) {
+        if (s1.length() == 0 || s2.length() == 0) return 0;
+
+        int[][] dp = new int[s1.length()][s2.length()];
+        for (int i = 0; i < s1.length(); i++) {
+            for (int j = 0; j < s2.length(); j++) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    if (i > 0 && j > 0) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    } else {
+                        dp[i][j] = 1;
+                    }
+                } else {
+                    if (i > 0) {
+                        dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+                    }
+                    if (j > 0) {
+                        dp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
+                    }
+                }
+            }
+        }
+        return dp[s1.length() - 1][s2.length() - 1];
+    }
 }
