e26dd812c785a26c6d9f053ac71e183795e8c501
quekx <syqkx123@163.com>
2017-06-23 13:11:05 +0800

    341

---
+++ src/com/qkx/example/solutions/LeetCode/medium/No341.java
@@ -0,0 +1,49 @@
+package com.qkx.example.solutions.LeetCode.medium;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by qkx on 17/6/23.
+ */
+public class No341 {
+    public class NestedIterator implements Iterator<Integer> {
+
+        private Stack<NestedInteger> stack;
+
+        public NestedIterator(List<NestedInteger> nestedList) {
+            stack = new Stack<>();
+            for (int i = nestedList.size() - 1; i >= 0; i--) {
+                stack.push(nestedList.get(i));
+            }
+        }
+
+        @Override
+        public Integer next() {
+            return stack.pop().getInteger();
+        }
+
+        @Override
+        public boolean hasNext() {
+            while (!stack.isEmpty()) {
+                NestedInteger nestedInteger = stack.peek();
+                if (nestedInteger.isInteger()) return true;
+
+                stack.pop();
+                List<NestedInteger> list = nestedInteger.getList();
+                for (int i = list.size() - 1; i >= 0; i--) {
+                    stack.push(list.get(i));
+                }
+            }
+
+            return false;
+        }
+    }
+
+    private interface NestedInteger {
+        boolean isInteger();
+        Integer getInteger();
+        List<NestedInteger> getList();
+    }
+}
