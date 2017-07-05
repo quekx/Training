b3a6c5da174bd0bcede8689aee6772ea6c02e12e
quekx <syqkx123@163.com>
2017-07-05 23:32:19 +0800

    ...

--- src/com/qkx/example/solutions/LeetCode/medium/No372.java
+++ src/com/qkx/example/solutions/LeetCode/medium/No372.java
@@ -7,6 +7,22 @@ import java.util.Map;
  * Created by qkx on 17/7/3.
  */
 public class No372 {
+    /**
+     * super pow
+     *
+     * Your task is to calculate ab mod 1337 where a is a positive integer
+     * and b is an extremely large positive integer given in the form of an array.
+     *
+     * Example1:
+     * a = 2
+     * b = [3]
+     * Result: 8
+     *
+     * Example2:
+     * a = 2
+     * b = [1,0]
+     * Result: 1024
+     */
     public static int superPow(int a, int[] b) {
         if (b == null || b.length == 0 || b[0] == 0) return 0;
 
