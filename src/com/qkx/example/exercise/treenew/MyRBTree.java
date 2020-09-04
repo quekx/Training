f0ff818d64f92fcd6724a09a8f4c8928bc099a3d
开忄 <syqkx123@163.com>
2020-09-04 14:27:12 +0800

    update delete in rbtree

--- src/com/qkx/example/exercise/treenew/MyRBTree.java
+++ src/com/qkx/example/exercise/treenew/MyRBTree.java
@@ -69,47 +69,102 @@ public class MyRBTree {
         }
     }
 
+    /**
+     * 删除调整
+     * @param x
+     * @param xp
+     */
     private void deleteFixUp(RBTreeNode x, RBTreeNode xp) {
         if (root == null || xp == null) return;
 
+        // 退出调整循环条件：当前x为红或者x调整至根节点
+        // 情况1：不进入循环
         while ((x == null || x.color == RBTreeNode.Color.BLACK) && x != root) {
+            // x为作子节点
             if (x == xp.left) {
                 RBTreeNode xr = xp.right;
-                if (xp.color == RBTreeNode.Color.RED) {
-                    xr.color = RBTreeNode.Color.RED;
-                    x = rotateLeft(xp);
-                } else if (xr.color == RBTreeNode.Color.RED) {
-                    RBTreeNode d = xr.left;
-                    d.color = RBTreeNode.Color.RED;
-                    x = rotateLeft(xp);
+                RBTreeNode a = xr.left;
+                RBTreeNode b = xr.right;
+                if (xr.color == RBTreeNode.Color.BLACK) {
+                    // 兄弟节点为黑
+                    // 情况2、情况3、情况4、情况5、情况6、情况7
+                    if (a == null || a.color == RBTreeNode.Color.BLACK) {
+                        // 情况2、情况5
+                        // xp左旋
+                        // 将x指向xr
+                        // 将xr(x)颜色设置为xp颜色
+                        // 情况2：xp为红，调整完成，x置为红退出循环
+                        // 情况5：xp为黑，需要继续下一轮调整
+                        rotateLeft(xp);
+                        x = xr;
+                        xr.color = xp.color;
+                    } else if (b != null && b.color == RBTreeNode.Color.RED) {
+                        // 情况3，情况6
+                        // xp左旋
+                        // 将xr颜色置为xp颜色（对应情况3、情况6）
+                        // xp颜色置黑
+                        // 两种情况均调整完成，x置红，退出循环
+                        rotateLeft(xp);
+                        xr.color = xp.color;
+                        xp.color = RBTreeNode.Color.BLACK;
+                        x.color = RBTreeNode.Color.RED;
+                    } else if (b == null || b.color == RBTreeNode.Color.BLACK) {
+                        // 情况4，情况7
+                        // xr右旋
+                        // a红置黑
+                        // xr黑置红
+                        // 调整至情况2、情况5，继续下一轮调整
+                        rotateRight(xr);
+                        a.color = RBTreeNode.Color.BLACK;
+                        xr.color = RBTreeNode.Color.RED;
+                    }
                 } else {
+                    // 兄弟节点为红，父节点必为黑
+                    // 情况8
+                    // 左旋xp
+                    // xp置红
+                    // xr置黑
+                    // 此时x的父节点xp变为红色，切换至情况2或情况3或情况4，继续下一轮调整
+                    rotateLeft(xp);
                     xp.color = RBTreeNode.Color.RED;
-                    x = rotateLeft(xp);
+                    xr.color = RBTreeNode.Color.BLACK;
                 }
-                if (xp == root) root = xr;
             } else {
+                // 对称情况
                 RBTreeNode xl = xp.left;
-                if (xp.color == RBTreeNode.Color.RED) {
-                    xl.color = RBTreeNode.Color.RED;
-                    x = rotateRight(xp);
-                } else if (xl.color == RBTreeNode.Color.RED) {
-                    RBTreeNode d = xl.right;
-                    d.color = RBTreeNode.Color.RED;
-                    x = rotateRight(xp);
+                RBTreeNode a = xl.left;
+                RBTreeNode b = xl.right;
+                if (xl.color == RBTreeNode.Color.BLACK) {
+                    if (b == null || b.color == RBTreeNode.Color.BLACK) {
+                        rotateRight(xp);
+                        x = xl;
+                        xl.color = xp.color;
+                    } else if (a != null && a.color == RBTreeNode.Color.RED) {
+                        rotateRight(xp);
+                        xl.color = xp.color;
+                        xp.color = RBTreeNode.Color.BLACK;
+                        x.color = RBTreeNode.Color.RED;
+                    } else if (a == null || a.color == RBTreeNode.Color.BLACK) {
+                        rotateRight(xl);
+                        b.color = RBTreeNode.Color.BLACK;
+                        xl.color = RBTreeNode.Color.RED;
+                    }
                 } else {
+                    rotateRight(xp);
                     xp.color = RBTreeNode.Color.RED;
-                    x = rotateRight(xp);
+                    xl.color = RBTreeNode.Color.BLACK;
                 }
-                if (xp == root) root = xl;
             }
-            xp = x.parent;
+            // 如果x指向新节点，更新xp
+            xp = x != null ? x.parent : xp;
         }
         x.color = RBTreeNode.Color.BLACK;
     }
 
     /**
      * 用x的子节点移植取代x节点
-     * @param x 被取代的节点
+     *
+     * @param x  被取代的节点
      * @param xc 子节点
      */
     private void transplant(RBTreeNode x, RBTreeNode xc) {
@@ -236,6 +291,10 @@ public class MyRBTree {
             }
         }
 
+        if (x == root) {
+            root = xr;
+        }
+
         return xr;
     }
 
@@ -256,6 +315,10 @@ public class MyRBTree {
             } else {
                 xp.right = xl;
             }
+        }
+
+        if (x == root) {
+            root = xl;
         }
 
         return xl;
