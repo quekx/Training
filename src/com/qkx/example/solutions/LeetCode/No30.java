b65151bf425874a7db40326e6d2b3f516a553b24
开忄 <syqkx123@163.com>
2021-01-13 20:32:45 +0800

    hard no23 merge k sorted list

---
+++ src/com/qkx/example/solutions/LeetCode/No30.java
@@ -0,0 +1,63 @@
+package com.qkx.example.solutions.LeetCode;
+
+import java.util.List;
+
+/**
+ * @author kaixin
+ * @since 2021-01-13 17:31
+ */
+//You are given a string s and an array of strings words of the same length. Ret
+//urn all starting indices of substring(s) in s that is a concatenation of each wo
+//rd in words exactly once, in any order, and without any intervening characters.
+//
+//
+// You can return the answer in any order.
+//
+//
+// Example 1:
+//
+//
+//Input: s = "barfoothefoobarman", words = ["foo","bar"]
+//Output: [0,9]
+//Explanation: Substrings starting at index 0 and 9 are "barfoo" and "foobar" re
+//spectively.
+//The output order does not matter, returning [9,0] is fine too.
+//
+//
+// Example 2:
+//
+//
+//Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+//Output: []
+//
+//
+// Example 3:
+//
+//
+//Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+//Output: [6,9,12]
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= s.length <= 104
+// s consists of lower-case English letters.
+// 1 <= words.length <= 5000
+// 1 <= words[i].length <= 30
+// words[i] consists of lower-case English letters.
+//
+// Related Topics Hash Table Two Pointers String
+// 👍 1117 👎 1388
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class No30 {
+    public List<Integer> findSubstring(String s, String[] words) {
+
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
