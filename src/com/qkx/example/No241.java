5ef0b1e37107b3d07bad2889f7d18ada8d1600b9
开忄 <syqkx123@163.com>
2021-11-03 18:04:06 +0800

    no299

---
+++ src/com/qkx/example/No241.java
@@ -0,0 +1,14 @@
+package com.qkx.example;
+
+import java.util.List;
+
+/**
+ * @author kaixin
+ * @since 2021-11-02 11:32
+ */
+public class No241 {
+    public List<Integer> diffWaysToCompute(String expression) {
+
+        return null;
+    }
+}
