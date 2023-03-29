e5bfcb1fdef2c5e02e60b5288892f0b4f9567e79
kaixin <syqkx123@163.com>
2023-03-29 15:03:57 +0800

    no50

--- src/com/qkx/example/lc/medium/No4x_5x/No50.java
+++ src/com/qkx/example/lc/medium/No4x_5x/No50.java
@@ -1,5 +1,7 @@
 package com.qkx.example.lc.medium.No4x_5x;
 
+import java.util.Set;
+
 /**
  * Created by qkx on 16/5/20.
  */
@@ -8,10 +10,19 @@ public class No50 {
      * Implement pow(x, n).
      */
     public double myPow(double x, int n) {
+        long m = n;
+        return m >= 0 ? pow(x, m) : 1 / pow(x, -m);
+    }
+
+    private double pow(double x, long n) {
         if (n == 0) {
             return 1;
         }
-//        for ()
-        return 0;
+        if (n == 1) {
+            return x;
+        }
+        double a = pow(x, n / 2);
+        double ans = a * a;
+        return n % 2 == 0 ? ans : ans * x;
     }
 }
